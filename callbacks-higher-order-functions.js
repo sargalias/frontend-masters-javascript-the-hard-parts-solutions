4c851d1d5ee86f1b3938b634a6af5a5bed546499
Spyros Argalias <sargalias@gmail.com>
2019-07-28 16:23:08 +0100

    feat(callbacks): Complete addTwo function

--- callbacks-higher-order-functions.js
+++ callbacks-higher-order-functions.js
@@ -4,12 +4,12 @@ console.log('Hello, world!');
 
 // Challenge 1
 function addTwo(num) {
-
+  return num + 2;
 }
 
 // To check if you've completed it, uncomment these console.logs!
-// console.log(addTwo(3));
-// console.log(addTwo(10));
+console.log(addTwo(3));
+console.log(addTwo(10));
 
 
 // Challenge 2
