d07488edcf8f88da41d666d660b0b0e22cfe1ab7
Spyros Argalias <sargalias@gmail.com>
2019-07-28 16:23:31 +0100

    feat(callbacks): Complete addS function

--- callbacks-higher-order-functions.js
+++ callbacks-higher-order-functions.js
@@ -14,12 +14,12 @@ console.log(addTwo(10));
 
 // Challenge 2
 function addS(word) {
-
+  return word + 's';
 }
 
 // uncomment these to check your work
-// console.log(addS('pizza'));
-// console.log(addS('bagel'));
+console.log(addS('pizza'));
+console.log(addS('bagel'));
 
 
 // Challenge 3
