32cf2612762470e5db9d67db72f9a958c117feda
Spyros Argalias <sargalias@gmail.com>
2019-07-29 05:57:00 +0100

    feat(closures): Complete after function

--- closures-scope-executionContext.js
+++ closures-scope-executionContext.js
@@ -96,15 +96,21 @@ console.log(onceFunc(9001));  //should log 6
 
 
 function after(count, func) {
-
+  let counter = 0;
+  return function(...args) {
+    counter++;
+    if (counter >= count) {
+      return func(...args);
+    }
+  }
 }
 
 var called = function () { console.log('hello') };
 var afterCalled = after(3, called);
 
-// afterCalled(); // -> nothing is printed
-// afterCalled(); // -> nothing is printed
-// afterCalled(); // -> 'hello' is printed
+afterCalled(); // -> nothing is printed
+afterCalled(); // -> nothing is printed
+afterCalled(); // -> 'hello' is printed
 
 
 function delay(func, wait) {
