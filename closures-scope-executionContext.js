8cd63b0a5d944eefd1c72be405f4316fec7698c4
Spyros Argalias <sargalias@gmail.com>
2019-07-29 05:48:22 +0100

    feat(closures): Complete challenge 3

--- closures-scope-executionContext.js
+++ closures-scope-executionContext.js
@@ -45,12 +45,12 @@ var jasCounter = outer();
 // Uncomment each of these lines one by one.
 // Before your do, guess what will be logged from each function call.
 
-// willCounter();
-// willCounter();
-// willCounter();
+willCounter();
+willCounter();
+willCounter();
 
-// jasCounter();
-// willCounter();
+jasCounter();
+willCounter();
 
 
 
